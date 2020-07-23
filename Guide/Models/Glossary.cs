124311dce5467cba79745f37b59e739c296a3cd0
Gleb Chuprin <chuperstory@gmail.com>
2020-07-23 17:47:43 +0600

    #4 Реализовал сущность Glossary

---
+++ Guide/Models/Glossary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Guide.Models
+{
+    public class Glossary
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
