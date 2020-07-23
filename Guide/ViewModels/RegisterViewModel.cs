5ec75dc6acbb7ff5f57222b60c729c9a6fe9ac21
Gleb Chuprin <chuperstory@gmail.com>
2020-07-23 23:07:19 +0600

    #5 добавил ViewModels : ChangePasswordViewModel, RegisterViewModel, LoginViewModel

---
+++ Guide/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guide.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [EmailAddress(ErrorMessage = "Некорректный тип Email-a")]
+        [Remote("CheckEmail", "Validation", ErrorMessage = "Этот Email занят")] 
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+}
