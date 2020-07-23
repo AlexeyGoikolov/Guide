293f88d36959c2e0cf3136d5b2ea42c5db399b5b
Gleb Chuprin <chuperstory@gmail.com>
2020-07-23 17:47:59 +0600

    #4 Реализовал сущность QuestionAnswer

---
+++ Guide/Models/QuestionAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Guide.Models
+{
+    public class QuestionAnswer
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public List<string> Links { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
