dc3434a26351cdea063460ca129feb9c33835391
Gleb Chuprin <chuperstory@gmail.com>
2020-07-23 23:03:24 +0600

    #5 Добавил AccountController : Action Index, Action Login, Action Register, Action Edit, Action Logout, Action ChangePassword

---
+++ Guide/Controllers/AccountController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Threading.Tasks;
+using Guide.Models;
+using Guide.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace Guide.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IHostEnvironment _environment;
+        
+
+
+        public AccountController(
+            UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<User> signInManager,
+            IHostEnvironment environment
+            )
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _environment = environment;
+        }
+
+        
+
+        [Authorize]
+        public IActionResult Index(string id = null)
+        {
+            User user = id == null? _userManager.GetUserAsync(User).Result : _userManager.FindByIdAsync(id).Result;
+            return View(user);
+        }
+        
+        
+        
+        
+        [Authorize]
+        public IActionResult Edit(string id = null)
+        {
+            User user = new User();
+            if (User.IsInRole("admin"))
+            {
+                user = id == null? _userManager.GetUserAsync(User).Result : _userManager.FindByIdAsync(id).Result;
+            }
+            else
+            {
+                user = _userManager.FindByIdAsync(id).Result;
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Edit(User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(userModel.Id);
+                if (user != null)
+                {
+                    user.Email = userModel.Email;
+                    
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user is null)
+                return NotFound();
+            ChangePasswordViewModel model = new ChangePasswordViewModel()
+            {
+                Id = user.Id,
+                Email = user.Email
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var passwordValidator = HttpContext.RequestServices.GetService(typeof(IPasswordValidator<User>)) as IPasswordValidator<User>;
+                    var passwordHasher = HttpContext.RequestServices.GetService(typeof(IPasswordHasher<User>)) as IPasswordHasher<User>;
+                    var result = await passwordValidator.ValidateAsync(_userManager, user, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user, model.NewPassword);
+                        await _userManager.UpdateAsync(user);
+                        return RedirectToAction("Index");
+                    }
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError("NewPassword", error.Description);
+                }
+                ModelState.AddModelError("", "Пользователь не существует");
+            }
+
+            return View(model);
+        }
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Email = model.Email,
+                    UserName = model.Email,
+                    
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Account");
+                }
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(String.Empty, error.Description);
+            }
+            return View(model);
+        }
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel {ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByEmailAsync(model.Email);
+                var result = await _signInManager.PasswordSignInAsync(
+                    user,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl)&&
+                        Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+
+                    return RedirectToAction("Index", "Account");
+                }
+                ModelState.AddModelError("", "Неправильный логин или пароль");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+    }
+}
