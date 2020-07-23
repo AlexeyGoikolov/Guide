eab2d6269eca7e3a74ed1b79d632a45e439dee82
Gleb Chuprin <chuperstory@gmail.com>
2020-07-23 17:48:05 +0600

    #4 Реализовал сущность Type

---
+++ Guide/Models/Type.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Guide.Models
+{
+    public class Type
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+    }
+}
