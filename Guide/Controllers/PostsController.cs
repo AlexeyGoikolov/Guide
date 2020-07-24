ed27d25fa6d8c62606e56f66ccb5a0dfbaa8e5dd
Gleb Chuprin <chuperstory@gmail.com>
2020-07-24 18:31:16 +0600

    #14 Реализовал не полное добавление сущности Post

---
+++ Guide/Controllers/PostsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Guide.Models;
+using Guide.Models.Data;
+using Guide.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guide.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly GuideContext _db;
+
+        public PostsController(GuideContext db)
+        {
+            _db = db;
+        }
+        // GET
+        public IActionResult Create()
+        {
+            return View(new Post());
+        }
+
+        
+    }
+}
