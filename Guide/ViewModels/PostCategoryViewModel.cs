1b94ef178dcad810e5d77087a71cf6468fc22b28
Gleb Chuprin <chuperstory@gmail.com>
2020-07-24 18:34:30 +0600

    #11 Добавил viewModel PostCategory для добавления категорий

---
+++ Guide/ViewModels/PostCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Guide.Models;
+
+namespace Guide.ViewModels
+{
+    public class PostCategoryViewModel
+    {
+        public Post Post { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
