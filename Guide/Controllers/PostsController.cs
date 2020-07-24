3907972c15fcc99203941d1980ab151b70067046
Gleb Chuprin <chuperstory@gmail.com>
2020-07-24 18:33:25 +0600

    #11 Добавил функионал добавления категорий

--- Guide/Controllers/PostsController.cs
+++ Guide/Controllers/PostsController.cs
@@ -20,6 +20,40 @@ namespace Guide.Controllers
             return View(new Post());
         }
 
+        public IActionResult CreateCategoryAjax(Category category)
+        {
+            if (category.Name != null)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+            }
+            PostCategoryViewModel model = new PostCategoryViewModel()
+            {
+                Post = new Post(),
+                Categories = _db.Categories.ToList()
+            };
+            
+            return PartialView("PartialViews/CategoriesPartial", model);
+        }
+        
+        
+        
+        public IActionResult DeleteCategoryAjax(string id)
+        {
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+            }
+            PostCategoryViewModel model = new PostCategoryViewModel()
+            {
+                Post = new Post(),
+                Categories = _db.Categories.ToList()
+            };
+            
+            return PartialView("PartialViews/CategoriesPartial", model);
+        }
         
     }
 }
