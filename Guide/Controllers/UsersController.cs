7740a4c2919f6a5391ff2b2f655c781d9b499bce
Lotov <lotovmuhtar@gmail.com>
2020-07-26 12:48:37 +0600

    #16 Реализован Фильтр списка всех пользователей по статусу и позициям

--- Guide/Controllers/UsersController.cs
+++ Guide/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Guide.Models;
 using Guide.Models.Data;
+using Guide.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,10 +19,38 @@ namespace Guide.Controllers
 
         public  IActionResult ListUsers()
         {
-           
-            List<User> users = _db.Users.Include(p=>p.Position).Where(u => u.Email != "admin@admin.com").ToList();
-            
+            List<User> listUsers = Filter(null,null);
+            ListUsersViwModel users = new ListUsersViwModel()
+            {
+                Users = listUsers,
+                Positions = _db.Positions.ToList()
+            };
             return View(users);
+        }
+        [HttpPost]
+        public  IActionResult ListUsers(ListUsersViwModel usersViwModel)
+        {
+            List<User> listUsers = Filter(usersViwModel.Action,usersViwModel.idPosition);
+            ListUsersViwModel users = new ListUsersViwModel()
+            {
+                Users = listUsers,
+                Positions = _db.Positions.ToList()
+            };
+            return View(users);
+        }
+        public List<User> Filter(string? activ, string? idPositions)
+        {
+            IQueryable<User> users = _db.Users.Include(p => p.Position).Where(u => u.Email != "admin@admin.com");
+            if (activ != null)
+            {
+                if (activ == "true")
+                    users = users.Where(u => u.Active);
+                if (activ == "false")
+                    users = users.Where(u => u.Active == false);
+            }
+            if (idPositions != null)
+                users = users.Where(u => u.PositionId == idPositions);
+            return users.ToList();
         }
     }
 }
---
+++ Guide/ViewModels/ListUsersViwModel.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using Guide.Models;
+
+namespace Guide.ViewModels
+{
+    public class ListUsersViwModel
+    {
+        public string? Action { get; set; }
+        public string? idPosition { get; set; }
+        
+        public List<User> Users { get; set; }
+        public List<Position> Positions { get; set; }
+    }
+}
