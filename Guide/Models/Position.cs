ebbc257548e495a520fe88b31abccf9ec5cd1bc1
Lotov <lotovmuhtar@gmail.com>
2020-07-26 10:49:07 +0600

    #15 добавлен модель Position.cs     добавлены строки в модель User     string Surname     string PositionId     virtual Position Position     DateTime DateCreate

---
+++ Guide/Models/Position.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Guide.Models
+{
+    public class Position
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+    }
+}
--- Guide/Models/User.cs
+++ Guide/Models/User.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace Guide.Models
 {
     public class User : IdentityUser
     {
+        public string Surname { get; set; }
+        
+        public string PositionId { get; set; }
+        public virtual Position Position { get; set; }
         public bool Active { get; set; } = true;
+        
+        public DateTime DateCreate { get; set; } = DateTime.Now;
+
     }
 }
