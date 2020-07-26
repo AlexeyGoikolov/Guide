646b05f825430faffae6198c979716a65ecb92c2
Lotov <lotovmuhtar@gmail.com>
2020-07-26 15:50:37 +0600

    #17 Редактирование,отключение и подключение пользователя в списке всех пользователей

--- Guide/Controllers/UsersController.cs
+++ Guide/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Guide.Models;
 using Guide.Models.Data;
 using Guide.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,15 +13,17 @@ namespace Guide.Controllers
     public class UsersController : Controller
     {
         private readonly GuideContext _db;
+        private readonly UserManager<User> _userManager;
 
-        public UsersController(GuideContext db)
+        public UsersController(GuideContext db, UserManager<User> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
 
         public  IActionResult ListUsers()
         {
-            List<User> listUsers = Filter(null,null);
+            List<User> listUsers = Filter("true",null);
             ListUsersViwModel users = new ListUsersViwModel()
             {
                 Users = listUsers,
@@ -38,6 +42,7 @@ namespace Guide.Controllers
             };
             return View(users);
         }
+        [NonAction]
         public List<User> Filter(string? activ, string? idPositions)
         {
             IQueryable<User> users = _db.Users.Include(p => p.Position).Where(u => u.Email != "admin@admin.com");
@@ -51,6 +56,30 @@ namespace Guide.Controllers
             if (idPositions != null)
                 users = users.Where(u => u.PositionId == idPositions);
             return users.ToList();
+        }
+        [HttpPost]
+        public async  Task<IActionResult> BlockingAjax(string idUser)
+        {
+            User user = _db.Users.FirstOrDefault(u => u.Id == idUser);
+            if (user != null)
+            {
+                if (user.Active)
+                {
+                    user.Active = false;
+                    await _userManager.UpdateAsync(user);
+                    return Json(false);
+                }
+                else
+                {
+                    user.Active = true;
+                    await _userManager.UpdateAsync(user);
+                    return Json(true);
+                }
+            }
+            else
+                return NotFound();
+            
+           
         }
     }
 }
