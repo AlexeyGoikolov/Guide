a2f10d3fb53957cac7517aee007395324fb6e2be
Gleb Chuprin <chuperstory@gmail.com>
2020-07-30 18:37:29 +0600

    #26 Добавил Model Book, ViewModel BookCreate

---
+++ Guide/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Guide.Models
+{
+    public class Book
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string CoverPath { get; set; }
+        public string VirtualPath { get; set; }
+        public string PhysicalPath { get; set; }
+        public string Type { get; set; } = "2";
+        public DateTime DateOfWriting { get; set; }
+        public DateTime DateCreate { get; set; } = DateTime.Now;
+        public DateTime DateUpdate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Guide/ViewModels/BookCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Guide.ViewModels
+{
+    public class BookCreateViewModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public IFormFile CoverPath { get; set; }
+        public IFormFile VirtualPath { get; set; }
+        public string PhysicalPath { get; set; }
+        public DateTime DateOfWriting { get; set; }
+    }
+}
