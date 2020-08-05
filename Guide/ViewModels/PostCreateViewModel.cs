00523f22d33aae2bd40d94be69a97d480cc64144
Галина Дугина <galkaz1977@gmail.com>
2020-08-05 19:47:21 +0600

    #31 добавила валидацию при создании Материала

--- Guide/ViewModels/PostCreateViewModel.cs
+++ Guide/ViewModels/PostCreateViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Guide.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
@@ -9,12 +10,21 @@ namespace Guide.ViewModels
     public class PostCreateViewModel
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required(ErrorMessage = "Необходимо заполнить поле Заголовок")]
+        [DataType(DataType.Text)]
+        
         public string Title { get; set; }
+        [Required(ErrorMessage = "Необходимо заполнить информацию об Авторе")]
+        [DataType(DataType.Text)]
         public string Author { get; set; }
         public string TextContent { get; set; }
         public IFormFile VirtualPath { get; set; }
         public string PhysicalPath { get; set; }
+         [Required(ErrorMessage = "Необходимо заполнить поле Категория")]
+                [DataType(DataType.Text)]
         public string CategoryId { get; set; }
+        [Required(ErrorMessage = "Необходимо заполнить поле Тип")]
+        [DataType(DataType.Text)]
         public string TypeId { get; set; }
     }
 }
