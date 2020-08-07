ef22b2035a1072c5fbb2f0651afc29db6d379770
Алексей Гойколов <goikolov.a.d@gmail.com>
2020-08-08 03:28:38 +0600

    #39 Добавлены Title к блоку Вопрос-ответ. Подправлены представления

--- Guide/ViewModels/QuestionAnswersViewModel.cs
+++ Guide/ViewModels/QuestionAnswersViewModel.cs
@@ -5,14 +5,14 @@ namespace Guide.ViewModels
     public enum QuestionAnswersStatus
     {
         Edit,
-        Creat,
+        Create,
         ToAnswer,
         ToQuestion
     }
     public class QuestionAnswersViewModel
     {
         public QuestionAnswer QuestionAnswer { get; set; } = new QuestionAnswer();
-        public QuestionAnswersStatus Status { get; set; } = QuestionAnswersStatus.Creat;
+        public QuestionAnswersStatus Status { get; set; } = QuestionAnswersStatus.Create;
         public string PostId { get; set; }
         public  Post Post { get; set; }
     }
