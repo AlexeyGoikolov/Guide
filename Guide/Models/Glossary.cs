fcf21d96acbcee183181e54dc05443bd4c90f65b
Алексей Гойколов <goikolov.a.d@gmail.com>
2020-08-13 01:08:34 +0600

    #47 Подправил формы добавления и редактирования глоссария. Добавил стили для кнопок в style.css

--- Guide/Models/Glossary.cs
+++ Guide/Models/Glossary.cs
@@ -7,11 +7,11 @@ namespace Guide.Models
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        [Required(ErrorMessage = "Поля не заполнено")]
+        [Required(ErrorMessage = "Поле не заполнено")]
         [MaxLength(25,ErrorMessage = "Максимальная длина названия 25 символов")]
 
         public string Name { get; set; }
-        [Required(ErrorMessage = "Поля не заполнено")]
+        [Required(ErrorMessage = "Поле не заполнено")]
         public string Description { get; set; }
         public string Abbreviation { get; set; }
         public string Source { get; set; }
