6f93571bc6db7fb19937ec180431ea0e9d2928d5
Галина Дугина <galkaz1977@gmail.com>
2020-08-16 22:27:27 +0600

    сделала для пользователя просмотр списка книг, одной книги и чтение

--- Guide/Controllers/BooksController.cs
+++ Guide/Controllers/BooksController.cs
@@ -13,7 +13,45 @@ namespace Guide.Controllers
 {
     public class BooksController : Controller
     {
+  private readonly GuideContext _db;
+
+  public BooksController(GuideContext db)
+  {
+      _db = db;
+  }
+
+
+  public IActionResult Index(string activ)
+        {
+            if (activ == null)
+            {
+                return View(_db.Books.Where(b => b.Active).ToList());
+            }
+            return View(_db.Books.Where(b => b.Active == false).ToList());
+        }
         
-        
+       
+
+        public IActionResult Details(int id)
+        {
+            Book book = _db.Books.FirstOrDefault(b => b.Id == id);
+            return View(book);
+        }
+     public  IActionResult ReadBook(string path, int id)
+        {
+            
+            if (path != null)
+            {
+                string ext=path.Substring(path.LastIndexOf('.'));
+                if (ext == ".pdf")
+                {
+                    Book book = _db.Books.FirstOrDefault(b => b.Id == id);
+                    return View(book) ;
+                }
+                
+            }
+
+            return NotFound() ;
+        }
     }
 }
