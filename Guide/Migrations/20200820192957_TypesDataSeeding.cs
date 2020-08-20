7960ea41eca3dbe8ddac0894cfe848e7d69f6e6e
Алексей Гойколов <goikolov.a.d@gmail.com>
2020-08-21 02:30:32 +0600

    #54 Добавлена возможность просмотра материалов и книг по нажатию на иконку вид пользователя. Подкорректирована детальная страница книги

---
+++ Guide/Migrations/20200820192957_TypesDataSeeding.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Guide.Migrations
+{
+    public partial class TypesDataSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                "Types",
+                new[] {"Id", "Name", "Active"},
+                new object[]{1, "Книга", true }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                "Types",
+                new[] {"Id", "Name", "Active"},
+                new object[]{1, "Книга", true }
+            );
+        }
+    }
+}
