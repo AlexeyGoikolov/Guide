e60b2ff70d3d462c83efdc257efacc792c2b6b3f
Gleb Chuprin <chuperstory@gmail.com>
2020-08-28 00:36:49 +0600

    #62 Создал сущность Толкование : содержит поля: описание и аббревиатура, источник, и ID термина

---
+++ Guide/Models/Interpretation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Permissions;
+
+namespace Guide.Models
+{
+    public class Interpretation
+    {
+        public int Id { get; set; }
+        public int GlossaryId { get; set; }
+        [Required(ErrorMessage = "Поле не заполнено")]
+        public string Description { get; set; }
+        public string Abbreviation { get; set; }
+        public string Source { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
