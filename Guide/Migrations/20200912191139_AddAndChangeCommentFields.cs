1515c1be717f264c015040b766c7f6cca9d7ecc1
Алексей Гойколов <goikolov.a.d@gmail.com>
2020-09-13 01:19:20 +0600

    #84 Изменил модель Comment. Добавил миграцию. Добавил картинку отсутствия обложки.

---
+++ Guide/Migrations/20200912191139_AddAndChangeCommentFields.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Guide.Migrations
+{
+    public partial class AddAndChangeCommentFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "SelText",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostId",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "Comments",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_BookId",
+                table: "Comments",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Books_BookId",
+                table: "Comments",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Books_BookId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_BookId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostId",
+                table: "Comments",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SelText",
+                table: "Comments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
