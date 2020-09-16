60329b5151419d9a9dc27069230148881015adba
Галина Дугина <galkaz1977@gmail.com>
2020-09-16 23:38:23 +0600

    #90 Добавила в Глоссарий дополнительные поля

---
+++ Guide/Migrations/20200916170621_GlossaryAddFields.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Guide.Migrations
+{
+    public partial class GlossaryAddFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GlossarysId",
+                table: "Glossaries",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Language",
+                table: "Glossaries",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GlossarysId",
+                table: "Glossaries");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Glossaries");
+        }
+    }
+}
