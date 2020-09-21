95af1c454ecbb7ebd5bc28c1f4cf3b24f90926f4
Галина Дугина <galkaz1977@gmail.com>
2020-09-21 21:15:17 +0600

    #95 Добавила контроллер и страницу просмотра для Бизнесс процессов на стороне пользователя

---
+++ Guide/Controllers/BusinessProcessController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Guide.Models;
+using Guide.Models.Data;
+using Guide.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guide.Controllers
+{
+    public class BusinessProcessController : Controller
+    {
+        // GET
+        private readonly GuideContext _db;
+
+        public BusinessProcessController(GuideContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Details(int id)
+        {
+            BusinessProcessIssuesViewModel model = new BusinessProcessIssuesViewModel()
+            {
+                BusinessProcess = _db.BusinessProcesses.FirstOrDefault(i => i.Id == id),
+                DesignatedIssues = _db.BusinessProcessIssues.OrderBy(b => b.Id).Where(b => b.BusinessProcessId == id).
+                    Select(i => i.Issue).ToList(),
+                AllIssue = new List<Issue>(),
+            };
+            return View(model);
+        }
+    }
+}
