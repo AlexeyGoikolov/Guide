ae148f846fd0ddd11e852608407eaa2cf349af2f
Lotov <lotovmuhtar@gmail.com>
2020-09-25 14:33:54 +0600

    #100 Добавить в создание книг, возможность добавления ключевых слов, связь с БП и между русской и английской версией книг. Создал BookBusinessProcess.cs связывающую таблицу для связи между книгами и БП, и BookIdAndEnglishBookId.cs книгами на разных языках. Lotov 20 minutes ago

--- Guide/Areas/Admin/Controllers/BooksManageController.cs
+++ Guide/Areas/Admin/Controllers/BooksManageController.cs
@@ -1,10 +1,7 @@
 ﻿
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Guide.Models;
 using Guide.Models.Data;
 using Guide.Services;
