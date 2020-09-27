7db00b3cef7983280ee70f2ac4596a6716d2283f
Галина Дугина <galkaz1977@gmail.com>
2020-09-27 21:11:27 +0600

    #104 миграции : добавление полей с ключевыми словами для Книги и Источника

---
+++ Guide/Migrations/20200927151003_fieldsKeys.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Guide.Migrations
+{
+    public partial class fieldsKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Keys",
+                table: "Posts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Keys",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Keys",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Keys",
+                table: "Books");
+        }
+    }
+}
